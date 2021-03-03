ca973221967dec1aef9d3dea65b3ea39405d46bb
Fabian Conejo <fabianconejosegu@gmail.com>
2021-03-02 21:00:45 -0600

    Refactored html divs and styled the view.

--- random.js
+++ random.js
@@ -1,4 +1,4 @@
-const alumns = ["Sofia Cárdenas", "Jorge Medrano", "Mariangela Alejandrina Pérez Lugo", "Fabián Conejo", "Shir Sahagún", "Julian Barrios ", "Angel Antonio", "Lucia Auyón", "Andru Vargas", "Fernando Hernandez Baez", "Arturo Camargo"];
+const alumns = ["Sofia Cárdenas", "Jorge Medrano", "Mariangela Alejandrina Pérez Lugo", "Fabián Conejo", "Shir Sahagún", "Julian Barrios ", "Angel Antonio", "Lucia Auyón", "Andru Vargas", "Fernando Hernandez Baez", "Arturo Camargo", "Miguel Monterroso", "Gerson González", "Oscar Hernández", "Laura Meléndez", "Sofia Fernandez", "Ana Maria Herrera", "Ignacio Gómez", "Dany Parc", "Andy Morales", "Alejandra Garcia", "Sherihan Garcia", "Mario Luis Valdez Maciel", "Víctor Alfonso Muñoz Toro", "Victor Cárdenas", "Rodrigo Eduardo Delgadillo González (Roy)", "Daniel Reyes Vivar", "Fernanda Escobedo", "Jorge Alvarez", "Isaac Castro", "Miguel Rodriguez", "Diana Ortiz"];
 
 const randomPicker = (array) => {
     let randNum = Math.floor(Math.random() * array.length);
@@ -7,5 +7,5 @@ const randomPicker = (array) => {
 };
 
 $('.random').click(function() {
-    document.getElementById('name').innerHTML = `${randomPicker(alumns)}`;
+    document.getElementById('card-body').innerHTML = `${randomPicker(alumns)}`;
 });
